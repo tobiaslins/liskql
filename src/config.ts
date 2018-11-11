00d991d02d43757ee7477ad6d58cbe69cb7105c5
Tobias Lins <me@tobi.sh>
2018-11-11 15:07:49 +0100

    fix stuff

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,8 @@
+require('dotenv').config();
+
+console.log(process.env.LISK_DB_HOST);
+console.log(process.env.ENV_LISK_DB_HOST);
+
 export const config = {
   connection: {
     host: process.env.LISK_DB_HOST,
